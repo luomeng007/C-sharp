0b38b68baa65e6ccb8d0b066e7832d8da600e86f
Luo Meng <53254812+luomeng007@users.noreply.github.com>
2020-10-13 17:55:25 +0200

    Add files via upload

---
+++ mainProgram.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+// all kinds of c sharp anotations
+namespace basicUsage
+{
+    class mainProgram
+    {
+        static void Main(string[] args)
+        {
+            // method 1: parse method
+            string a = "10.55";
+
+            float b = Single.Parse(a);
+
+            Console.WriteLine(b);
+
+            // method 2: Convert method
+            string c = "47174.73";
+            
+            float d = Convert.ToSingle(c);
+            
+            Console.WriteLine(d);
+        }
+    }
+}
