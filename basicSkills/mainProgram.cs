6baae1ccec58a10497281eee5fd85c60198868a1
Luo Meng <53254812+luomeng007@users.noreply.github.com>
2020-10-13 18:02:26 +0200

    Add files via upload

---
+++ basicSkills/mainProgram.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+// convert type of data, when our system is using German.
+namespace basicUsage
+{
+    class mainProgram
+    {
+        static void Main(string[] args)
+        {
+            // method 1: parse method
+            string a = "10.55";
+
+            float b = Single.Parse(a, CultureInfo.InvariantCulture);
+
+            Console.WriteLine(b);
+
+            // method 2: Convert method
+            string c = "47174.73";
+            
+            float d = Convert.ToSingle(c, CultureInfo.InvariantCulture);
+            
+            Console.WriteLine(d);
+        }
+    }
+}
