a856c5645d6090e9e4bd1f0cb1a4ee2a8703596d
Luo Meng <53254812+luomeng007@users.noreply.github.com>
2020-12-03 16:42:01 +0100

    Update readFile.cs

--- basicSkills/readFile.cs
+++ basicSkills/readFile.cs
@@ -4,12 +4,26 @@ namespace readData
     {
         static void Main(string[] args)
         {
+            #region
             // info will store the full directory we give
             DirectoryInfo info = new DirectoryInfo(@"C:\documents\C#Project\Pro_W\researchData\downloadFile\releasedFile\");
             // Exists method will get a bool value, if directory exits, True. Attention, judge directory not file.
             Console.WriteLine(info.Exists);
+            #endregion
             
-            
+            #region
+            DirectoryInfo info = new DirectoryInfo(@"C:\Users\15025\Desktop\C#Debug");
+            // if directory not exists, create it
+            if (! info.Exists)
+            {
+                info.Create();
+            }
+            // if directory exists, delete it, the director must be empty
+            if (info.Exists)
+            {
+                info.Delete();
+            }
+            #endregion
         }
     }
 }
