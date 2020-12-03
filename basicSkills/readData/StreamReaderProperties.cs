3f9e549ad9686b72f3ae84576acc44e5001e4bc1
Luo Meng <53254812+luomeng007@users.noreply.github.com>
2020-12-03 18:04:42 +0100

    Update StreamReaderProperties.cs

--- basicSkills/readData/StreamReaderProperties.cs
+++ basicSkills/readData/StreamReaderProperties.cs
@@ -1,15 +1,21 @@
-static void Main(string[] args)
-        {
-           
+using System;
+using System.IO;
+using System.Text;
 
+namespace readData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
             // 属性与方法调用的不同是属性调用末尾无需括号，而方法调用需要括号
             //// three properties
             //// return the encoding method
-            //Console.WriteLine(str1.CurrentEncoding);
+            Console.WriteLine(str1.CurrentEncoding);
             //// return the underlying stream, if we read file, it should be filestream
-            //Console.WriteLine(str1.BaseStream);
+            Console.WriteLine(str1.BaseStream);
             //// check whether the current stream positon is at the end of the steam. return True or False value
-            //Console.WriteLine(str1.EndOfStream)
-                }
-            }  
+            Console.WriteLine(str1.EndOfStream)  
         }
+    }
+}
