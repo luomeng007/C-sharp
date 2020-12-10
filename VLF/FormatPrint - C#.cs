72890aa211432c4c53b0b068e8a4f0f594d95cf6
Luo Meng <53254812+luomeng007@users.noreply.github.com>
2020-12-10 10:04:14 +0100

    Add files via upload

---
+++ VLF/FormatPrint - C#.cs
@@ -0,0 +1,28 @@
+using System;
+
+using VirtualLab.Programming;
+using VirtualLabAPI.Core.BasicFunctions;
+using VirtualLabAPI.Core.Common;
+using VirtualLabAPI.Core.DataVisualization;
+using VirtualLabAPI.Core.FieldRepresentations;
+using VirtualLabAPI.Core.Functions;
+using VirtualLabAPI.Core.GeometryDescription;
+using VirtualLabAPI.Core.LightPath;
+using VirtualLabAPI.Core.Materials;
+using VirtualLabAPI.Core.Modules;
+using VirtualLabAPI.Core.Numerics;
+using VirtualLabAPI.Core.Numerics.Region2D;
+using VirtualLabAPI.Core.OpticalSystems;
+using VirtualLabAPI.Core.Propagation;
+using VirtualLabAPI.Core.SupportFunctions;
+
+namespace OwnCode {
+    public class VLModule {
+        public void Run() {
+            var x = 5;
+            // by using this method we could leave out data transfer by using .ToString at the end of program.
+            Globals.DataDisplay.LogMessage($"The value of x is {x}");
+        }
+    }
+}
+// The value of x is 5
