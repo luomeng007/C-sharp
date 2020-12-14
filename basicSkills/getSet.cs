0e9a6e604230963c7dd0725a01d54310e8f745c4
Luo Meng <53254812+luomeng007@users.noreply.github.com>
2020-12-14 17:01:03 +0100

    Create getSet.cs

---
+++ basicSkills/getSet.cs
@@ -0,0 +1,47 @@
+using System;
+
+namespace debug
+{
+    #region example one
+    //class Person
+    //{
+    //    private string name;
+    //    public string Name
+    //    {
+    //        get { return name; }
+    //        set { name = value; }
+    //    }
+    //}
+
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Person myObj = new Person();
+    //        myObj.Name = "Liam";
+    //        Console.WriteLine(myObj.Name);
+    //    }
+    //}
+    #endregion
+
+    #region example 2 less code
+    class Person
+    {
+
+        public string Name   // property
+        { get; set;}
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person myObj = new Person();
+            myObj.Name = "Liam";
+            Console.WriteLine(myObj.Name);
+        }
+    }
+    #endregion
+}
+//Result:
+//Liam
