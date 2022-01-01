b4cea802c2bba39a51c3dfb43daabc98ba2b4951
Luo Meng <53254812+luomeng007@users.noreply.github.com>
2022-01-01 09:34:51 +0100

    Create 构造函数---Constructor(副)Car01.cs

---
+++ advancedSkill/构造函数---Constructor(副)Car01.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace T_Constructor
+{
+    class Car01
+    {
+        public string model;
+
+        // 创建一个类的构造函数并且带有参数
+        public Car01(string modelName)
+        {
+            model = modelName;
+        }
+    }
+}
