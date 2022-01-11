ee3596ea488cd5d48c04fb463e7f455b9ba08400
Luo Meng <53254812+luomeng007@users.noreply.github.com>
2022-01-11 09:24:19 +0100

    Update T007_Keyword_GetLength.cs

--- basicSkills/T007_Keyword_GetLength.cs
+++ basicSkills/T007_Keyword_GetLength.cs
@@ -14,6 +14,7 @@ namespace CSharp_Tutorial
             Console.WriteLine("The shape of array2D is: ({0}, {1})", array2D.GetLength(0), array2D.GetLength(0));  // like keyword "shape" in Python
             // array2D.GetLength(0) represents the number of rows array2D contains
             // array2D.GetLength(1) represents the number of columns array2D contains
+            // extension: If we have a 3D array here, GetLength(2) represents the third dimension of array.
         }
     }
 }
