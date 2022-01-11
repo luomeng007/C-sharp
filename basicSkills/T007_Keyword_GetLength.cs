9398a461dbe22225c33dc240b28c04bf61104a9e
Luo Meng <53254812+luomeng007@users.noreply.github.com>
2022-01-11 09:22:35 +0100

    Create T007_Keyword_GetLength.cs

---
+++ basicSkills/T007_Keyword_GetLength.cs
@@ -0,0 +1,19 @@
+/*
+Description:
+    Aim: teach people the function of keyword "GetLength" in C Sharp.
+ */
+using System;
+
+namespace CSharp_Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array2D = new int[2,2];  // initialize a 2D array with 2*2=4 elements
+            Console.WriteLine("The shape of array2D is: ({0}, {1})", array2D.GetLength(0), array2D.GetLength(0));  // like keyword "shape" in Python
+            // array2D.GetLength(0) represents the number of rows array2D contains
+            // array2D.GetLength(1) represents the number of columns array2D contains
+        }
+    }
+}
