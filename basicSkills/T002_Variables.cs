75db31ffce0437a6c49fb64097e74d875bf3fbcf
Luo Meng <53254812+luomeng007@users.noreply.github.com>
2022-01-11 06:18:02 +0100

    Update T002_Variables.cs

--- basicSkills/T002_Variables.cs
+++ basicSkills/T002_Variables.cs
@@ -1,1 +1,21 @@
+/*
+Description:
+    Aim: teach people to know basic variables in c sharp
+ */
+using System;
 
+namespace CSharp_Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            Console.WriteLine(a);
+            float b = 5;
+            Console.WriteLine(b);
+            double c = 5;
+            Console.WriteLine(c);
+        }
+    }
+}
