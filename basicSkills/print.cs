9d53b5fbcdcccd7eaa39f11b7c3b0aced1447174
Luo Meng <53254812+luomeng007@users.noreply.github.com>
2022-01-11 06:13:24 +0100

    Create print.cs

---
+++ basicSkills/print.cs
@@ -0,0 +1,16 @@
+/*
+Description:
+  Aim: teach people how to print stuffs on screen.
+*/
+using System;
+
+namespace CSharp_Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
