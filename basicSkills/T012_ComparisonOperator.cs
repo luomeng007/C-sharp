6b84ab8407c6cb9aa843d613ae5dbc68d1a9d613
Luo Meng <53254812+luomeng007@users.noreply.github.com>
2022-01-12 02:55:56 +0100

    Create T012_ComparisonOperator.cs

---
+++ basicSkills/T012_ComparisonOperator.cs
@@ -0,0 +1,30 @@
+/*
+Description:
+    Aim: teach people compare two values by using comparision operator.
+ */
+using System;
+
+namespace CSharp_Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            int c = 1;
+
+            Console.WriteLine(a > b);   //  return False
+            Console.WriteLine(a < b);   //  return True
+            Console.WriteLine(a == c);  //  return True
+
+            float d = 1.1F;
+            float e = 2.0F;
+            float f = 1.1F;
+
+            Console.WriteLine(d > e);   //  return False
+            Console.WriteLine(d < e);   //  return True
+            Console.WriteLine(d == f);  //  return True
+        }
+    }
+}
