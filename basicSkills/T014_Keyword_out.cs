c633b52e9a5caf647aca727def6c923ea57eab77
Luo Meng <53254812+luomeng007@users.noreply.github.com>
2022-01-12 05:09:55 +0100

    Create T014_Keyword_out.cs

---
+++ basicSkills/T014_Keyword_out.cs
@@ -0,0 +1,30 @@
+/*
+Description:
+    Aim: teach people how to return value from customized function by using keyword "out".
+
+    
+ */
+using System;
+
+namespace CSharp_Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 10;
+            Add(a, b, out int sum);
+            // When we use "out" keyword, wo do not need to initialize it before we pass it to the calling function
+            // This differs from keyword "ref". Of course we can do it like the way below.
+            // int sum = 0; 
+            // Add(a, b, out sum);
+            Console.WriteLine("The value of sum of a and b is: {0}", sum);
+        }
+
+        public static void Add(int num1, int num2, out int num_sum)
+        {
+            num_sum = num1 + num2;  // this operation will influence the value of sum in main function 
+        }
+    }
+}
