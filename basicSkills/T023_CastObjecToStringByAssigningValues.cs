0d6261442cf7781aa6bade7ae0cd7e41ec830c44
Luo Meng <53254812+luomeng007@users.noreply.github.com>
2022-01-14 05:31:49 +0100

    Create T023_CastObjecToStringByAssigningValues.cs

---
+++ basicSkills/T023_CastObjecToStringByAssigningValues.cs
@@ -0,0 +1,26 @@
+/*
+Description:
+    Aim: teach people how to cast type object to type string by directly assigning values.
+ */
+using System;
+
+namespace CSharp_Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Usage two:
+            string str1 = "Xiao Ming";  // create str3           
+            object obj1 = str1; // assign str1 to obj1  
+            
+            // cast to(transfer) a string
+            // check
+            if (obj1 is string)
+            {
+                Console.WriteLine(obj1);
+                Console.WriteLine(obj1.GetType());
+            }
+        }
+    }
+}
