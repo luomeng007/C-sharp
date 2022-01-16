535013c66e649ddb24fb644a889f5a2f855ebe4f
Luo Meng <53254812+luomeng007@users.noreply.github.com>
2022-01-16 03:55:59 +0100

    Update and rename defaultValue.cs to T032_defaultValue.cs

--- basicSkills/T032_defaultValue.cs
+++ basicSkills/T032_defaultValue.cs
@@ -1,16 +1,19 @@
+/*
+Description:
+    Aim: teach people the default value of variables in c sharp is 0.
+ */
 using System;
 
-// assign default value to variables
-namespace basicUsage
+namespace CSharp_Tutorial
 {
-    class defaultValue
+    class Program
     {
         static void Main(string[] args)
         {
             int x = default;
             float x1 = default;
             double x2 = default;
-            
+
             Console.WriteLine("The value of x is {0}", x);
             Console.WriteLine("The value of x1 is {0}", x1);
             Console.WriteLine("The value of x2 is {0}", x2);
