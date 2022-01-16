23b925c071e6d65bac4906ab9b930b5fb3043755
Luo Meng <53254812+luomeng007@users.noreply.github.com>
2022-01-16 04:24:06 +0100

    Update T001_Print.cs

--- basicSkills/T001_Print.cs
+++ basicSkills/T001_Print.cs
@@ -2,15 +2,15 @@
 Description:
   Aim: teach people how to print stuffs on screen.
 */
-using System;
+using System; // Illustrate that users can define namespace now
 
-namespace CSharp_Tutorial
+namespace CSharp_Tutorial // Define a name space
 {
-    class Program
+    class Program // define a class
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // define main program, we also call it as entry point
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");  // print "Hello World!" on screen
         }
     }
 }
